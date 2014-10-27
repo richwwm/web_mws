ef5ed99420af79a8d0fcb8f39782ad8bcd94bdf0
ozcaryyh <ozcaryyh@gmail.com>
2014-10-27 23:10:29 +0800

    no use items

--- MWSWebSolution/LabelPrintingInterface/Site1.Master.cs
+++ MWSWebSolution/LabelPrintingInterface/Site1.Master.cs
@@ -12,7 +12,21 @@ namespace LabelPrintingInterface
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            GetUser_IP();
+        }
 
+        protected void GetUser_IP()
+        {
+            string VisitorsIPAddr = string.Empty;
+            if (HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null)
+            {
+                VisitorsIPAddr = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
+            }
+            else if (HttpContext.Current.Request.UserHostAddress.Length != 0)
+            {
+                VisitorsIPAddr = HttpContext.Current.Request.UserHostAddress;
+            }
+            ClientInfoLabel.Text = "Your IP is: " + VisitorsIPAddr;
         }
     }
 }
